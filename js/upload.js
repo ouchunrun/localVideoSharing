5ba549978aef2fb92f3274a6db3a9a8dedcdd788
ouchunrun <1402327769@qq.com>
2018-04-13 10:57:20 +0800

    本地添加到git

---
+++ js/upload.js
@@ -0,0 +1,49 @@
+var imgURL = "";
+var leftVideo = document.getElementById('leftVideo');
+var rightVideo = document.getElementById('rightVideo');
+var test = document.getElementById("test");
+
+function getImgURL(node) {
+    let file = null;
+    try{
+        if(node.files && node.files[0] ){
+            file = node.files[0];
+        }else if(node.files && node.files.item(0)) {
+            file = node.files.item(0);
+        }
+
+        try{
+            //Firefox7.0
+            imgURL =  file.getAsDataURL();
+        }catch(e){
+            //Firefox8.0以上
+            imgRUL = window.URL.createObjectURL(file);
+        }
+
+        let spstr = file.name.split(".");
+        let fileType = spstr[spstr.length-1];
+        if(fileType === "mp4" || fileType === "webm" || fileType === "mp3"){
+            leftVideo.src = window.URL.createObjectURL(file);
+        }
+
+    }catch(e){      //这里不知道怎么处理了，如果是遨游的话会报这个异常
+        //支持html5的浏览器,比如高版本的firefox、chrome、ie10
+        if (node.files && node.files[0]) {
+            let reader = new FileReader();
+            reader.onload = function (e) {
+                imgURL = e.target.result;
+            };
+            reader.readAsDataURL(node.files[0]);
+        }
+    }
+    creatImg(imgRUL);
+    return imgURL;
+}
+
+function creatImg(imgRUL){   //根据指定URL创建一个Img对象
+    var textHtml = "<img src='"+imgRUL+"'/>";
+    $(".mark").after(textHtml);
+}
+
+
+
