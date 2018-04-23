8dd354dda6f72191ff6ecd08c9b406679e73778b
Lenovo-PC\chrou <1402327769@qq.com>
2018-04-23 10:40:36 +0800

    还是无法实现视频的分辨率等的约束

--- js/upload.js
+++ js/upload.js
@@ -22,24 +22,26 @@ uploadFile.addEventListener("change", function () {
         let typeJudge = file.type.split("/")[0];
         if(typeJudge === "audio" || typeJudge === "video"){
             leftV.src = fileURL;
+            printInfo(file);
         }else if(typeJudge === "image"){
             if (file.type !== 'image/jpeg' && file.type !== 'image/png' && file.type !== 'image/gif') {
                 alert('不是有效的图片文件!');
                 return;
             }
             setCanvas(fileURL);     // setCanvas() is in the main.js
+            printInfo(file);
         }else {
             console.log("请上传视频、音频、图片！！暂不支持别的applications");
         }
-
-        // 打印上传的文件信息
-        fileInfo.innerHTML = '文件: ' + file.name + '<br>' +
-            '大小: ' + file.size + '<br>' +
-            '修改: ' + file.lastModifiedDate;
-
     }catch(e){
-        // 兼容问题很大，暂时不考虑兼容
         console.log(e.message);
     }
 });
+
+// 打印上传的文件信息
+function printInfo(file) {
+    fileInfo.innerHTML = '文件: ' + file.name + '<br>' +
+        '大小: ' + file.size + '<br>' +
+        '修改: ' + file.lastModifiedDate;
+}
 
